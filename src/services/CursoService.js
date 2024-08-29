e51ee80db868ea92917fc2cced4524f1c00bb506
fellypemartins <fellypemartins.official@gmail.com>
2024-08-29 13:38:46 -0300

    Add createCursos

--- src/services/CursoService.js
+++ src/services/CursoService.js
@@ -1,9 +1,23 @@
 const database = require('../database')
 
 module.exports = {
+    //Ler Cursos
     readCursos: () => {
         return new Promise((resolve, reject) => {
             database.query('SELECT * FROM curso', (err, result) => {
+                if (err) {
+                    reject(err)
+                    return
+                }
+                return resolve(result)
+            })
+        })
+    },
+
+    //Criar Cursos
+    createCursos: (nome) => {
+        return new Promise((resolve, reject) => {
+            database.query(`INSERT INTO curso VALUES (null, ${nome}, null)`, (err, result) => {
                 if (err) {
                     reject(err)
                     return
